93dfc1ec15dbc1e2dd2771ee0372de4bdf34c0e7
MeltpoolMartin <martinwachter@gmx.net>
2021-03-27 15:58:06 +0100

    Update pihole compose file with shim network that docker host itself can be tracked

---
+++ pi-hole/shim_network.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ip link add macvlan_shim link ovs_eth0 type macvlan mode bridge
+ip addr add 192.168.1.21/32 dev macvlan_shim
+ip link set macvlan_shim up
+ip route add 192.168.1.20 dev macvlan_shim
